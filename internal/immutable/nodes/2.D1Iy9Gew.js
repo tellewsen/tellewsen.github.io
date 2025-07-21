54ec4f5c39e30ba8c100f11e9b0cb202808b3eca
Andreas Ellewsen <andreas.ellewsen@itera.com>
2025-07-21 08:53:23 +0200

    Updates

---
+++ internal/immutable/nodes/2.D1Iy9Gew.js
@@ -0,0 +1,3 @@
+import"../chunks/Bzak7iHL.js";import"../chunks/tpv3LOhO.js";import{f as s,a,n as i}from"../chunks/aRVO8S3z.js";var o=s(`<title>Ellewsen</title> <div><h1>Andreas Ellewsen</h1> <p>Welcome to the site. As you can see this site is pretty barren, and that's the way I like it.
+		The most interesting thing as of now is probably the utilities page. I use this page to test new
+		javascript frameworks. The current iteration of the site is written in <a href="https://svelte.dev/" target="_blank">Svelte</a> as a client side only page so that it can be hosted for free using Github Pages.</p> <p>Check out the <a href="/posts">posts</a> section for updates</p></div>`,1);function h(t){var e=o();i(2),a(t,e)}export{h as component};
--- internal/immutable/nodes/2.DM7aCpBB.js
+++
@@ -1,3 +0,0 @@
-import"../chunks/Bzak7iHL.js";import"../chunks/tpv3LOhO.js";import{f as s,a}from"../chunks/aRVO8S3z.js";var i=s(`<div><h1>Andreas Ellewsen</h1> <p>Welcome to the site. As you can see this site is pretty barren, and that's the way I like it.
-		The most interesting thing as of now is probably the utilities page. I use this page to test new
-		javascript frameworks. The current iteration of the site is written in <a href="https://svelte.dev/" target="_blank">Svelte</a> as a client side only page so that it can be hosted for free using Github Pages.</p> <p>Check out the <a href="/posts">posts</a> section for updates</p></div>`);function p(t){var e=i();a(t,e)}export{p as component};
