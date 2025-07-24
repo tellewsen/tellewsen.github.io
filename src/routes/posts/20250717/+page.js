31a23eda80c2322e86b9026ddfca3ef791293fc5
Andreas Ellewsen <andreas@ellewsen.no>
2025-07-24 12:58:18 +0200

    Fix date of wifi qr post

--- src/routes/posts/20250717/+page.js
+++ src/routes/posts/20250717/+page.js
@@ -2,7 +2,7 @@ export const load = () => {
   return {
     metadata: {
       title: "Wifi mode for qr codes",
-      date: "2025-07-16T08:45:00+02:00",
+      date: "2025-07-17T19:45:00+02:00",
     }
   };
 };
