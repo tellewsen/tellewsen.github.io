280001f52a32ee337287fdaa31388b8c55f9b542
Andreas Ellewsen <andreas@ellewsen.no>
2025-07-25 20:11:59 +0200

    Add post for todays updates

---
+++ src/routes/posts/20250725/+page.js
@@ -0,0 +1,8 @@
+export const load = () => {
+  return {
+    metadata: {
+      title: "More utils",
+      date: "2025-07-25T20:00:00+02:00",
+    }
+  };
+};
---
+++ src/routes/posts/20250725/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Timestamp from '$lib/Timestamp.svelte';
+	export let data;
+</script>
+
+<article>
+	<h1>{data.metadata.title}</h1>
+	<small><Timestamp iso={data.metadata.date} /></small>
+	<hr />
+	<p>
+		The site got a few new utils today. Most of it is just thrown together by Copilot and ChatGPT to
+		get a better feel of what different models are capable of. They can at least do simple stuff,
+		but making it correct on the first attempt usually fails, and anything slightly complicated
+		rarely works well.
+	</p>
+	<p>
+		I also updated the nav bar, making it its own component instead of just having a nav tag on the
+		layout page.
+	</p>
+</article>
